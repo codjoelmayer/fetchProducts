670465410fdbc81443413caba680156ec8f5b8e4
codjoelmayer <tmjoelm4@gmail.com>
2022-12-19 17:19:16 +0200

    Working on a store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,24 @@
 import { createStore } from 'vuex'
 
+// URL
+const productUrl = "https://codjoelmayer.github.io/productsAPI/data.json";
 export default createStore({
   state: {
+    products: null
   },
   getters: {
   },
   mutations: {
+    setProducts(state, products) {
+      state.products = products;
+    }
   },
   actions: {
+    fetchProducts : async (context)=> {
+      const res = await fetch(`${productUrl}`);
+      const results = await res.json();
+      context.commit('setProducts', results);
+    }
   },
   modules: {
   }
